951476558cbe159b9389efe6e3415e7318cf16d8
MrArthur_exe <contato.arthurmorais.dev@gmail.com>
2023-01-02 23:44:22 -0300

    Explanation update

--- 1- Singleton/Singleton.java
+++ 1- Singleton/Singleton.java
@@ -1,8 +1,9 @@
 public class Singleton {
-    // É importante notar que o construtor é privado
+    // A instância no singleton deve ser privada.
     private static Singleton instance = null;
 
-    // Quando criamos o método getSingleton, ele verifica se já existe uma instância, se não existir, ele cria uma nova instância.
+    // Quando criamos o método getSingleton, ele verifica se já existe uma instância,
+    // se não existir, é criada uma nova instância
     public static Singleton getSingleton(){
         if(instance == null){
             System.out.println("Instancia criada com sucesso!");
